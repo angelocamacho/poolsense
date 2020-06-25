3a03c0ba133a8a0757df24dcc12d1ee803ceb317
Haemish Kyd <haemish.kyd@gmail.com>
2020-06-25 22:04:50 +0200

    Added some exception handling.

--- poolsense/__init__.py
+++ poolsense/__init__.py
@@ -1,11 +1,12 @@
 from aiohttp import ClientSession, ClientResponseError
+from poolsense.exceptions import PoolSenseError
 
 
 class PoolSense:
     """Main Interface to the Poolsense Device"""
 
     def __init__(self):
-        self._version = "0.0.4"
+        self._version = "0.0.5"
         self._url_login = 'https://api.poolsense.net/api/v1/users/login'
 
 
@@ -41,42 +42,49 @@ class PoolSense:
             "registrationId": "c5tknccIS_I:APA91bF0LS4mAR2NETBJ9tNFYEbvUgileRovnuC1Y9-yTy2qDsW4_YHlDcapH7BnHWzxh74fPVJw0Y9KuM3sCVIWknSOlGu3WP0QNSFzfuhEwQ_yBujt9cSVak0eVUo_IfmFf6rtlng_"
         }
 
+        results = {
+            "Chlorine": 0,
+            "pH": 0,
+            "Water Temp": 0,
+            "Battery": 0,
+            "Last Seen": 0,
+            "Chlorine High": 0,
+            "Chlorine Low": 0,
+            "pH High": 0,
+            "pH Low": 0,
+            "pH Status": 0,
+            "Chlorine Status": 0
+        }
+
         # """Login to the system."""
         resp = await session.post(self._url_login, json=LOGIN_DATA)
-        data = await resp.json(content_type=None)
+        if resp.status == 200:
+            data = await resp.json(content_type=None)
 
-        URL_DATA = 'https://api.poolsense.net/api/v1/sigfoxData/app/' + data['devices'][0]["serial"] + '/?tz=-120'
-        head = {'Authorization': 'token {}'.format(data["token"])}
+            URL_DATA = 'https://api.poolsense.net/api/v1/sigfoxData/app/' + data['devices'][0]["serial"] + '/?tz=-120'
+            head = {'Authorization': 'token {}'.format(data["token"])}
 
-        #
-        resp = await session.get(URL_DATA, headers=head)
-        data = await resp.json(content_type=None)
-        #     # API does not set correct status header so we manually check.
-        #     raise ClientResponseError(
-        #         resp.request_info,
-        #         resp.history,
-        #         status=data['error']['code'],
-        #         message=data['error']['message'],
-        #         headers=resp.headers
-        #     )
-
-        results = {
-            "Chlorine": data["ORP"],
-            "pH": data["pH"],
-            "Water Temp": data["waterTemp"],
-            "Battery": data["vBat"],
-            "Last Seen": data["lastData"]["time"],
-            "Chlorine High": data["display"]["orpNotificationMax"],
-            "Chlorine Low": data["display"]["orpNotificationMin"],
-            "pH High": data["display"]["phNotificationMax"],
-            "pH Low": data["display"]["phNotificationMin"],
-            "pH Status": data["display"]["pHColor"],
-            "Chlorine Status": data["display"]["ORPColor"]
-        }
-        # for item in data["features"]:
-        #     try:
-        #         results.append(source.from_json(item))
-        #     except KeyError:
-        #         logging.getLogger(__name__).warning("Got wrong data: %s", item)
+            #
+            resp = await session.get(URL_DATA, headers=head)
+            if resp.status == 200:
+                data = await resp.json(content_type=None)
+                
+                results = {
+                    "Chlorine": data["ORP"],
+                    "pH": data["pH"],
+                    "Water Temp": data["waterTemp"],
+                    "Battery": data["vBat"],
+                    "Last Seen": data["lastData"]["time"],
+                    "Chlorine High": data["display"]["orpNotificationMax"],
+                    "Chlorine Low": data["display"]["orpNotificationMin"],
+                    "pH High": data["display"]["phNotificationMax"],
+                    "pH Low": data["display"]["phNotificationMin"],
+                    "pH Status": data["display"]["pHColor"],
+                    "Chlorine Status": data["display"]["ORPColor"]
+                }
+            else:
+                raise PoolSenseError(resp.status,"Server error.")
+        else:
+            raise PoolSenseError(resp.status,"Login failed.")
 
         return results
